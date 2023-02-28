0b1fd076a18e6bc821f47d29119ecec2ee0f44ac
laehoonlee <xeo@uiscloud.net>
2023-02-28 16:01:35 +0900

    MD : MD-38 페이징 정리

--- src/main/java/com/mslk/sns/department/controller/DepartmentController.java
+++ src/main/java/com/mslk/sns/department/controller/DepartmentController.java
@@ -1,9 +1,13 @@
 package com.mslk.sns.department.controller;
 
+import com.mslk.common.paging.Pagination;
+import com.mslk.common.paging.dto.SearchDto;
 import com.mslk.sns.department.dto.DepartmentDto;
 import com.mslk.sns.department.service.DepartmentService;
 import com.mslk.sns.staff.dto.StaffDto;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -17,20 +21,33 @@ import java.util.List;
 public class DepartmentController {
     private DepartmentService departmentService;
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
 
     /* 게시글 목록 */
 
     @GetMapping("/snsad/departmentlist")
-    public String index(Model model, @RequestParam(value="page", defaultValue = "1") Integer pageNum) {
-        List<DepartmentDto> departmentlist = departmentService.getDepartmentlist(pageNum);
-        Integer[] pageList = departmentService.getPageList(pageNum);
+    public String index(Model model, @ModelAttribute("params") final SearchDto params) {
+        List<DepartmentDto> departmentlist = departmentService.getDepartmentlist(params.getPage());
 
         double  count = Double.valueOf(departmentService.getDepartmentCount());
         Integer postsTotalCount = (int) count;
 
+        logger.info("params : " + params.getPage());
+
+
+        Pagination pagination = new Pagination(postsTotalCount, params);
+
+        logger.info("totalRecordCount : " + pagination.getTotalRecordCount());
+        logger.info("totalPageCount : " + pagination.getTotalPageCount());
+        logger.info("startPage : " + pagination.getStartPage());
+        logger.info("endPage : " + pagination.getEndPage());
+        logger.info("limitStart : " + pagination.getLimitStart());
+        logger.info("existPrevPage : " + pagination.isExistPrevPage());
+        logger.info("existNextPage : " + pagination.isExistNextPage());
+
         model.addAttribute("departmentlist", departmentlist);
-        model.addAttribute("pageList", pageList);
-        model.addAttribute("postsTotalCount", postsTotalCount);
+        model.addAttribute("pagination", pagination);
 
         return "sns/department/list";
     }
