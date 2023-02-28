73267cf10fbf5eaaaba896d4e9d74f58da98c249
laehoonlee <xeo@uiscloud.net>
2023-02-28 17:21:05 +0900

    MD : MD-41 페이징 기능 정리

--- src/main/java/com/mslk/sns/position/controller/PositionController.java
+++ src/main/java/com/mslk/sns/position/controller/PositionController.java
@@ -1,9 +1,13 @@
 package com.mslk.sns.position.controller;
 
+import com.mslk.common.paging.Pagination;
+import com.mslk.common.paging.dto.SearchDto;
 import com.mslk.sns.department.dto.DepartmentDto;
 import com.mslk.sns.position.dto.PositionDto;
 import com.mslk.sns.position.service.PositionService;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -17,20 +21,33 @@ import java.util.List;
 public class PositionController {
     private PositionService positionService;
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
 
     /* 게시글 목록 */
 
     @GetMapping("/snsad/positionlist")
-    public String index(Model model, @RequestParam(value="page", defaultValue = "1") Integer pageNum) {
-        List<PositionDto> positionlist = positionService.getPositionlist(pageNum);
-        Integer[] pageList = positionService.getPageList(pageNum);
+    public String index(Model model, @ModelAttribute("params") final SearchDto params) {
+        List<PositionDto> positionlist = positionService.getPositionlist(params.getPage());
 
         double  count = Double.valueOf(positionService.getPositionCount());
         Integer postsTotalCount = (int) count;
 
+        logger.info("params : " + params.getPage());
+
+
+        Pagination pagination = new Pagination(postsTotalCount, params);
+
+        logger.info("totalRecordCount : " + pagination.getTotalRecordCount());
+        logger.info("totalPageCount : " + pagination.getTotalPageCount());
+        logger.info("startPage : " + pagination.getStartPage());
+        logger.info("endPage : " + pagination.getEndPage());
+        logger.info("limitStart : " + pagination.getLimitStart());
+        logger.info("existPrevPage : " + pagination.isExistPrevPage());
+        logger.info("existNextPage : " + pagination.isExistNextPage());
+
         model.addAttribute("positionlist", positionlist);
-        model.addAttribute("pageList", pageList);
-        model.addAttribute("postsTotalCount", postsTotalCount);
+        model.addAttribute("pagination", pagination);
 
         return "sns/position/list";
     }
