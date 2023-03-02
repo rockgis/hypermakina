76897d1a48061dca4cb17dc1b266d63bf4445f40
laehoonlee <xeo@uiscloud.net>
2023-03-02 15:34:01 +0900

    MD : MD-56 접속 허용 IP 관리 페이징

--- src/main/java/com/mslk/hypermakina/ipusenm/controller/IpusenmController.java
+++ src/main/java/com/mslk/hypermakina/ipusenm/controller/IpusenmController.java
@@ -1,8 +1,12 @@
 package com.mslk.hypermakina.ipusenm.controller;
 
+import com.mslk.common.paging.Pagination;
+import com.mslk.common.paging.dto.SearchDto;
 import com.mslk.hypermakina.ipusenm.dto.Gittf0002Dto;
 import com.mslk.hypermakina.ipusenm.service.Gittf0002Service;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -16,19 +20,33 @@ import java.util.List;
 public class IpusenmController {
     private Gittf0002Service gittf0002Service;
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     /* Main Page */
     @GetMapping("/admin/ipusenm")
-    public String list(Model model, @RequestParam(value="page", defaultValue = "1") Integer pageNum) {
+    public String list(Model model, @ModelAttribute("params") final SearchDto params) {
 
-        List<Gittf0002Dto> gittf0002List = gittf0002Service.getGittf0002list(pageNum);
-        Integer[] pageList = gittf0002Service.getPageList(pageNum);
+        List<Gittf0002Dto> gittf0002List = gittf0002Service.getGittf0002list(params.getPage());
 
         double  count = Double.valueOf(gittf0002Service.getGitta002Count());
         Integer postsTotalCount = (int) count;
 
+        logger.info("params : " + params.getPage());
+
+
+        Pagination pagination = new Pagination(postsTotalCount, params);
+
+        logger.info("totalRecordCount : " + pagination.getTotalRecordCount());
+        logger.info("totalPageCount : " + pagination.getTotalPageCount());
+        logger.info("startPage : " + pagination.getStartPage());
+        logger.info("endPage : " + pagination.getEndPage());
+        logger.info("limitStart : " + pagination.getLimitStart());
+        logger.info("existPrevPage : " + pagination.isExistPrevPage());
+        logger.info("existNextPage : " + pagination.isExistNextPage());
+
         model.addAttribute("gittf0002List", gittf0002List);
-        model.addAttribute("pageList", pageList);
-        model.addAttribute("postsTotalCount", postsTotalCount);
+        model.addAttribute("pagination", pagination);
+
 
 
         return "manager/ipusenm/main.html";
