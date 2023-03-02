93a7cc430028ea42ef455e961fc7c3ae62bef21f
laehoonlee <xeo@uiscloud.net>
2023-03-02 09:51:11 +0900

    MD : MD-44 페이징 기능 정리

--- src/main/java/com/mslk/sns/rank/controller/RankController.java
+++ src/main/java/com/mslk/sns/rank/controller/RankController.java
@@ -1,8 +1,12 @@
 package com.mslk.sns.rank.controller;
 
+import com.mslk.common.paging.Pagination;
+import com.mslk.common.paging.dto.SearchDto;
 import com.mslk.sns.rank.dto.RankDto;
 import com.mslk.sns.rank.service.RankService;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -16,20 +20,33 @@ import java.util.List;
 public class RankController {
     private RankService rankService;
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
 
     /* 게시글 목록 */
 
     @GetMapping("/snsad/ranklist")
-    public String index(Model model, @RequestParam(value="page", defaultValue = "1") Integer pageNum) {
-        List<RankDto> ranklist = rankService.getRanklist(pageNum);
-        Integer[] pageList = rankService.getPageList(pageNum);
+    public String index(Model model, @ModelAttribute("params") final SearchDto params) {
+        List<RankDto> ranklist = rankService.getRanklist(params.getPage());
 
         double  count = Double.valueOf(rankService.getRankCount());
         Integer postsTotalCount = (int) count;
 
+        logger.info("params : " + params.getPage());
+
+
+        Pagination pagination = new Pagination(postsTotalCount, params);
+
+        logger.info("totalRecordCount : " + pagination.getTotalRecordCount());
+        logger.info("totalPageCount : " + pagination.getTotalPageCount());
+        logger.info("startPage : " + pagination.getStartPage());
+        logger.info("endPage : " + pagination.getEndPage());
+        logger.info("limitStart : " + pagination.getLimitStart());
+        logger.info("existPrevPage : " + pagination.isExistPrevPage());
+        logger.info("existNextPage : " + pagination.isExistNextPage());
+
         model.addAttribute("ranklist", ranklist);
-        model.addAttribute("pageList", pageList);
-        model.addAttribute("postsTotalCount", postsTotalCount);
+        model.addAttribute("pagination", pagination);
 
         return "sns/rank/list";
     }
